20832abe23a6dcc7c223828b6312bf3d843a3fd6
Barbara Choy <barbchoy@gmail.com>
2018-01-03 22:12:59 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,10 +1,7 @@
-#!/usr/bin/env ruby
-require_relative '../lib/greeting.rb'
-
-def greeting
-
-  name = gets.strip
+def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
 
-  puts "Hi! I'm HAL, what's your name?"
+puts "Hi! I'm HAL, what's your name?"
+name = gets.strip
+greeting(name)
